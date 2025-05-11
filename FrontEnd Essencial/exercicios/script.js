63c5b12bf30d729bbe5c4cf4b7312b47cf75faea
Lívia Raissinger <108821710+Livia9@users.noreply.github.com>
2025-05-11 18:45:23 -0300

    Add files via upload
    Upload de alguns arquivos da disciplina

---
+++ FrontEnd Essencial/exercicios/script.js
@@ -0,0 +1,34 @@
+// const nome = "Livia";
+// console.log(`Meu nome é ${nome}`);
+// const aluno= "Livia";
+// const curso="Residência em TIC";
+// console.log(`Seja bem-vindo(a) ${aluno}, vamos iniciar a aula de ${curso}`);
+
+// Crie um objeto aluno com as propriedades: [nome, idade, curso].
+// Crie um método apresentar() que retorne uma string com os dados do aluno.
+// nome=   "livia"
+// const aluno = {
+//     nome: "livia",
+//     idade: 20,
+//     curso: "Engenharia elétrica",
+//     apresentar: function() {
+//       return `Olá, meu nome é ${this.nome}, tenho ${this.idade} anos e estou no curso de ${this.curso}.`;
+//     }
+//   }
+//   console.log(aluno.apresentar());
+
+// Crie uma função dividir(a, b) que retorna a divisao de dois valores.
+
+// function dividir(a,b){
+//     if (b <= 0){
+//         return `Erro! Insira um numero maior que zero!`
+//     } else {
+// const divisao= a/b;
+// return `O resultado da divisão é: ${divisao}`;
+// }}
+// console.log(dividir(8,4));
+5 - Crie um array[alunos] com pelo menos 3 objetos contendo[nome e idade].
+Use map() para gerar um novo array apenas com os nomes dos alunos.
+Use filter() para retornar os alunos com idade maior que 18.
+
+// perdi o arquivo com o rest dos exercicios feitos
